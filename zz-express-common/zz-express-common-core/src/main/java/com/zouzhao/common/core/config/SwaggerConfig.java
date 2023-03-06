cb7fe047bc9cd411dd658bad204a6307e382cb12
yaoc <yaoc@landray.com.cn>
2023-03-06 14:56:23 +0800

    提交人：yaoc 项目：zz-express 模块：sys-org、opt-manage 描述：解决swagger问题

--- zz-express-common/zz-express-common-core/src/main/java/com/zouzhao/common/core/config/SwaggerConfig.java
+++ zz-express-common/zz-express-common-core/src/main/java/com/zouzhao/common/core/config/SwaggerConfig.java
@@ -1,21 +1,22 @@
-package com.zouzhao.common.config;
+package com.zouzhao.common.core.config;
 
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.AntPathMatcher;
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
-import java.util.function.Predicate;
 
 /**
  * @author 姚超
@@ -41,7 +42,8 @@ public class SwaggerConfig {
             Docket docket = (new Docket(DocumentationType.SWAGGER_2)).
                     groupName(moduleName).apiInfo(apiInfoBuilder.title(moduleName).build())
                     .select().apis(this.genSubPackage(moduleName))
-                    .paths(this.ant()).build();
+                    .paths(Predicates.or(Predicates.or(PathSelectors.ant("/data/**"), PathSelectors.ant("/api/**"))))
+                    .build();
             beanFactory.registerSingleton("swagger-"+moduleName, docket);
         });
     }
@@ -51,15 +53,4 @@ public class SwaggerConfig {
         return RequestHandlerSelectors.basePackage("com.zouzhao." + moduleName.replace("-", "."));
     }
 
-    public  Predicate<String> ant() {
-        Predicate<String> predicate = input -> {
-            AntPathMatcher matcher = new AntPathMatcher();
-            return matcher.match("/data/**", input);
-        };
-        predicate.or(input -> {
-            AntPathMatcher matcher = new AntPathMatcher();
-            return matcher.match("/api/**", input);
-        });
-        return predicate;
-    }
 }
